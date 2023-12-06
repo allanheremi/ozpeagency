fe245f03ff844922ff08fb9ba783e2736aada206
Allan Heremi <allanheremidev@gmail.com>
2023-12-06 17:13:31 +0100

    Style: px heading large screen

--- src/app/components/Heading.tsx
+++ src/app/components/Heading.tsx
@@ -4,7 +4,7 @@ const text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit'
 
 const Heading = () => {
   return (
-    <h1 className='p-4 text-3xl pt-44 pb-64 px-8'>
+    <h1 className='p-4 text-3xl pt-44 pb-64 px-8 md:px-16'>
       {text.toUpperCase()}
     </h1>
   )

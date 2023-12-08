c8381319699df4908dbbf2ea7226e8d7a0a37242
Allan Heremi <allanheremidev@gmail.com>
2023-12-08 11:14:09 +0100

    Style: Fix: sizing

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,18 +5,26 @@ import Footer from "../components/Footer";
 const page = () => {
   return (
     <>
-      <div>
+      <div className="overflow-hidden max-h-screen max-w-screen">
         <Header />
-        <div className="md:p-18 flex items-center justify-center p-8 ">
+        <div className="md:p-18 flex h-[76vh] items-center justify-center p-8 ">
           <div>
-            <h1 className="py-4 text-3xl">ABOUT US</h1>
-            <p className="text-white/80">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
-              maxime numquam neque quia sunt. Culpa ducimus illo excepturi
-              autem? Minima temporibus odit dolore aut ratione non fugiat facere
-              numquam nobis.
-            </p>
+
+            <div className="flex flex-col gap-16 md:gap-4 px-4 md:px-12">
+
+            <div>
+              <h1 className=" text-3xl">ABOUT US</h1>
+            </div>
+            <div>
+              <p className="text-white/80">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+                maxime numquam neque quia sunt. Culpa ducimus illo excepturi
+                autem? Minima temporibus odit dolore aut ratione non fugiat
+                facere numquam nobis.
+              </p>
+            </div>
           </div>
+        </div>
         </div>
 
         <Footer />
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,60 +1,31 @@
 import React from "react";
 import Header from "../components/Header";
 import Image from "next/image";
+import Footer from "../components/Footer";
 
 const page = () => {
   return (
     <>
-      <Header />
+    <div className="overflow-hidden max-h-screen max-w-screen">
 
-      <section className="flex h-[calc(50vh)] flex-col items-center justify-center p-8">
-        <h1 className="p-4 text-xl">CONTACT</h1>
-        <div>
+      <Header />
+      <section className="flex h-[76vh] flex-col items-center justify-center gap-16 p-8">
+        <h1 className=" text-2xl">CONTACT US</h1>
+        <div className="text-center">
           <address className="flex flex-col">
             <a href="mailto:webmaster@example.com" className="not-italic">
               ozpeagency@gmail.com
             </a>
-            <a href="tel:+46 76 29 89 752" className="font-style: normal-case">
+            {/* @DEV make phone not italic */}
+            <a href="tel:+46 76 29 89 752" className="">
               +46 76 29 89 752{" "}
             </a>
           </address>
-
-          <div className="absolute bottom-0 left-0 flex w-screen flex-row justify-around gap-16 p-8">
-            <a href="https://www.instagram.com/ozpeagency/" target="_blank">
-              <Image
-                src={"/instagramicon.png"}
-                height={42}
-                width={42}
-                alt="instagram"
-                className="hover:cursor-pointer"
-              />
-            </a>
-
-            <a href="https://twitter.com/al3xc00p3r" target="_blank">
-              <Image
-                src={"/X.png"}
-                height={42}
-                width={42}
-                alt="X"
-                className="hover:cursor-pointer"
-              />
-            </a>
-
-            <a
-              href="https://www.linkedin.com/in/petrus-%C3%B6zer-315644269/"
-              target="_blank"
-            >
-              <Image
-                src={"/linkedin.png"}
-                height={42}
-                width={42}
-                alt="linkedin"
-                className="hover:cursor-pointer"
-              />
-            </a>
-          </div>
         </div>
       </section>
+      <Footer />
+    </div>
+
     </>
   );
 };

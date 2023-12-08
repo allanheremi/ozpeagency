0c4b48eeb6b4eef9ae4afd450324a65e2c853659
Allan Heremi <allanheremidev@gmail.com>
2023-12-08 15:01:12 +0100

    Style: Fix: mobile overflow when nav open

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,16 +1,29 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Close } from "@mui/icons-material";
 import Link from "next/link";
 import { useMediaQuery } from "react-responsive";
-import { useState } from "react";
 
 const Header = () => {
   const [showDropDown, setShowDropDown] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
   const isMobile = useMediaQuery({ maxWidth: 640 });
+
+  // Function to toggle the mobile menu and update body overflow
+  const toggleMobileMenu = () => {
+    setShowMenu(!showMenu);
+    document.body.style.overflow = showMenu ? 'auto' : 'hidden';
+  };
+
+  // Clean up body overflow style when the component unmounts
+  useEffect(() => {
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
+
   return (
     <>
       {!isMobile && (
@@ -43,25 +56,21 @@ const Header = () => {
               {showDropDown && (
                 <div className="animate-fadeInUpShort absolute right-0 top-full flex flex-col justify-center text-center text-black">
                   <div className="text-xl">
-
-                  <div className=" border-black bg-white p-2 px-4 hover:bg-white/90 active:bg-white/60">
+                    <div className=" border-black bg-white p-2 px-4 hover:bg-white/90 active:bg-white/60">
                       <Link href={"/about"}>
                         <p>HOME</p>
                       </Link>
                     </div>
-                    
                     <div className="bg-white border-t border-black/50 p-2 px-4 hover:bg-white/90 active:bg-white/60 ">
                       <Link href={"/contact"}>
                         <p className="w-full">CONTACT</p>
                       </Link>
                     </div>
-
                     <div className="border-t border-black/50 bg-white p-2 px-4 hover:bg-white/90 active:bg-white/60">
                       <Link href={"/about"}>
                         <p>ABOUT</p>
                       </Link>
                     </div>
-                    
                   </div>
                 </div>
               )}
@@ -85,20 +94,20 @@ const Header = () => {
             <MenuIcon
               fontSize="large"
               className="hover:cursor-pointer"
-              onClick={() => setShowMenu(!showMenu)}
+              onClick={toggleMobileMenu}
             />
             {showMenu && (
               <div className="animate-fadeInUp fixed left-0 top-0 z-10 h-screen w-screen overflow-hidden bg-white">
                 <div className="absolute right-4 top-4">
                   <Close
-                    onClick={() => setShowMenu(false)}
+                    onClick={toggleMobileMenu}
                     className="text-black"
                     fontSize="large"
                   />
                 </div>
                 <div className="flex h-screen w-screen items-center justify-center ">
-                  <div className="flex items-center justify-center flex-col gap-8">
-                  <div className="w-[40vw]  text-center ">
+                  <div className="flex items-center justify-center flex-col gap-y-8">
+                    <div className="w-[40vw]  text-center ">
                       <Link href={"/"}>
                         <p className="text-2xl text-black  animate-slide-left slide-left">HOME</p>
                       </Link>

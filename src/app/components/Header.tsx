940ccdcb2d8c6d306e5f6e995408bf11a83b73f3
Allan Heremi <allanheremidev@gmail.com>
2023-12-08 13:48:17 +0100

    Fix: Add home to navigation

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -43,17 +43,25 @@ const Header = () => {
               {showDropDown && (
                 <div className="animate-fadeInUpShort absolute right-0 top-full flex flex-col justify-center text-center text-black">
                   <div className="text-xl">
-                    <div className="bg-white p-2 px-4 hover:bg-white/90 active:bg-white/60 ">
+
+                  <div className=" border-black bg-white p-2 px-4 hover:bg-white/90 active:bg-white/60">
+                      <Link href={"/about"}>
+                        <p>HOME</p>
+                      </Link>
+                    </div>
+                    
+                    <div className="bg-white border-t border-black/50 p-2 px-4 hover:bg-white/90 active:bg-white/60 ">
                       <Link href={"/contact"}>
-                        <p className="w-full">Contact</p>
+                        <p className="w-full">CONTACT</p>
                       </Link>
                     </div>
 
-                    <div className="border-t border-black bg-white p-2 px-4 hover:bg-white/90 active:bg-white/60">
+                    <div className="border-t border-black/50 bg-white p-2 px-4 hover:bg-white/90 active:bg-white/60">
                       <Link href={"/about"}>
-                        <p>About</p>
+                        <p>ABOUT</p>
                       </Link>
                     </div>
+                    
                   </div>
                 </div>
               )}
@@ -90,6 +98,11 @@ const Header = () => {
                 </div>
                 <div className="flex h-screen w-screen items-center justify-center ">
                   <div className="flex items-center justify-center flex-col gap-8">
+                  <div className="w-[40vw]  text-center ">
+                      <Link href={"/"}>
+                        <p className="text-2xl text-black  animate-slide-left slide-left">HOME</p>
+                      </Link>
+                    </div>
                     <div className="w-[40vw] text-center ">
                       <Link href={"/about"}>
                         <p className="text-2xl text-black animate-slide-right slide-right ">ABOUT</p>

bee898a8d63c327c19385c5982060801ca1c30cd
Allan Heremi <allanheremidev@gmail.com>
2023-12-08 14:55:46 +0100

    Fix: video attributes

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -3,16 +3,20 @@ import React from "react";
 const Main = () => {
   return (
     // @DEV padding here to show BG white top?
-    <video controls autoPlay={true} muted>
+    <video
+      autoPlay
+      muted
+      controlsList="nofullscreen nodownload noremoteplayback noplaybackrate" 
+      disablePictureInPicture
+      loop={true}
+      controls
+    >
       <source
         src="https://sweet.se/wp-content/uploads/2022/04/Sweet_Headervideo-2022-01.mp4"
         type="video/mp4"
       />
       Your browser does not support this video format
     </video>
-
-    
-
   );
 };
 

7a8a3976c4030d18ad0cc262038d7d24225af4d5
Allan Heremi <allanheremidev@gmail.com>
2023-12-08 15:32:38 +0100

    Fix: contact icons

--- src/app/components/Clients.tsx
+++ src/app/components/Clients.tsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 const Clients = () => {
   return (
     <>
-      <div className="bg-white text-black p-8 h-[88vh] flex grid-cols-2 flex-col justify-between">
-        <h1 className="flex justify-center text-2xl md:py-8 py-16">CLIENTS</h1>
-        <div className="flex justify-center grow">
-          <div className="grid grid-cols-3 gap-x-24 gap-y-24 md:gap-y-16 md:gap-x-48  bg-white text-center text-black md:mb-4">
+      <div className="flex h-[66vh] md:h-[50vh] grid-cols-2 flex-col justify-between bg-white p-8 text-black">
+        <h1 className="flex justify-center py-16 text-2xl md:py-8">CLIENTS</h1>
+        <div className="flex grow justify-center">
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-x-24 gap-y-24 bg-white text-center  text-black md:mb-4 md:gap-x-48 md:gap-y-16">
             <div>
               <Image
                 src={"/BK_Black_Logo.png"}
@@ -33,6 +33,30 @@ const Clients = () => {
                 alt="logo bored kitchen"
               />
             </div>
+         <div>
+              <Image
+                src={"/Hairbyem2.png"}
+                width={72}
+                height={72}
+                alt="logo bored kitchen"
+              /> 
+            </div>
+            {/* <div>
+              <Image
+                src={"/BK_Black_Logo.png"}
+                width={72}
+                height={72}
+                alt="logo bored kitchen"
+              />
+            </div>  */}
+            {/* <div>
+              <Image
+                src={"/Hairbyem2.png"}
+                width={72}
+                height={72}
+                alt="logo bored kitchen"
+              />
+            </div>
             <div>
               <Image
                 src={"/Hairbyem2.png"}
@@ -56,32 +80,7 @@ const Clients = () => {
                 height={72}
                 alt="logo bored kitchen"
               />
-            </div>
-            <div>
-              <Image
-                src={"/Hairbyem2.png"}
-                width={72}
-                height={72}
-                alt="logo bored kitchen"
-              />
-            </div>
-            <div>
-              <Image
-                src={"/BK_Black_Logo.png"}
-                width={72}
-                height={72}
-                alt="logo bored kitchen"
-              />
-            </div>
-            <div>
-              <Image
-                src={"/Hairbyem2.png"}
-                width={72}
-                height={72}
-                alt="logo bored kitchen"
-              />
-            </div>
-            
+            </div> */}
           </div>
         </div>
       </div>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,30 +2,38 @@ import React from "react";
 import Header from "../components/Header";
 import Image from "next/image";
 import Footer from "../components/Footer";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
 
 const page = () => {
   return (
     <>
-    <div className="overflow-hidden max-h-screen max-w-screen">
-
-      <Header />
-      <section className="flex h-[66vh] md:h-[76vh] flex-col items-center justify-center gap-16 p-8">
-        <h1 className=" text-2xl">CONTACT US</h1>
-        <div className="text-center">
-          <address className="flex flex-col">
-            <a href="mailto:webmaster@example.com" className="not-italic">
-              ozpeagency@gmail.com
-            </a>
-            {/* @DEV make phone not italic */}
-            <a href="tel:+46 76 29 89 752" className="">
-              +46 76 29 89 752{" "}
-            </a>
-          </address>
-        </div>
-      </section>
-      <Footer />
-    </div>
-
+      <div className="max-w-screen max-h-screen overflow-hidden">
+        <Header />
+        <section className="flex h-[66vh] flex-col items-center justify-center gap-16 p-8 md:h-[76vh]">
+          <h1 className=" text-left text-2xl">CONTACT US</h1>
+          <div className="text-left">
+            <address className="flex flex-col">
+              <div className="flex flex-col justify-around gap-4">
+                <a
+                  href="mailto:webmaster@example.com"
+                  className="not-italic active:text-white/60"
+                >
+                  <MailOutlineIcon fontSize="medium" className="mx-1"/> ozpeagency@gmail.com
+                </a>
+                {/* @DEV make phone not italic */}
+                <a
+                  href="tel:+46 76 29 89 752"
+                  className="not-italic active:text-white/60"
+                >
+                  <LocalPhoneOutlinedIcon fontSize="medium" className="mx-1"/> +46 76 29 89 752{" "}
+                </a>
+              </div>
+            </address>
+          </div>
+        </section>
+        <Footer />
+      </div>
     </>
   );
 };

ee4fbc7f9ff257898822c23fff17f0ea0ba702e3
Allan Heremi <allanheremidev@gmail.com>
2023-12-09 19:17:23 +0100

    Fix: resize font

--- src/app/components/Subheading.tsx
+++ src/app/components/Subheading.tsx
@@ -4,7 +4,7 @@ const Subheading = () => {
   return (
     <div>
       <h1 className="flex h-[100vh] items-center bg-white p-4 px-8 text-5xl text-black md:px-24">
-        <span className="lg:text-7xl md:text-5xl text-3xl">
+        <span className="lg:text-7xl md:text-5xl text-[1.5rem]">
           THE FEAR OF LONG WORDS IS CALLED <br />
           HIPPOPOTOMONSTROSESQUIPPEDALIOPHOBIA. <br /> <br /> LETS PUSH SOME
           BOUNDARIES TOGHETER <br/> AND FIND THE UNIQUE IN YOU.

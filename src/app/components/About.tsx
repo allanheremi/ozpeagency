8e2fac7d293a1c13353dfb6a5ed13c6c0622daca
Allan Heremi <allanheremidev@gmail.com>
2023-12-10 11:59:37 +0100

    Fix: font size about

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -13,17 +13,17 @@ const About = ({ content }: ContentProps) => {
   return (
     <div>
       <div className="flex flex-col whitespace-nowrap p-4 px-8 md:px-2">
-        <h4 className="w-full pb-2 text-left text-xl md:text-4xl">{content.heading}</h4>
-        <p className="w-full text-left text-xl md:text-3xl lg:text-4xl text-white/80">
+        <h4 className="w-full pb-2 text-left text-3xl md:text-4xl">{content.heading}</h4>
+        <p className="w-full text-left text-2xl md:text-3xl lg:text-4xl text-white/80">
           {content.breadTextOne}
         </p>
-        <p className="w-full text-left text-xl md:text-3xl lg:text-4xl text-white/80">
+        <p className="w-full text-left text-2xl md:text-3xl lg:text-4xl text-white/80">
           {content.breadTextTwo}
         </p>
-        <p className="w-full text-left text-xl md:text-3xl lg:text-4xl text-white/80">
+        <p className="w-full text-left text-2xl md:text-3xl lg:text-4xl text-white/80">
           {content.breadTextThree}
         </p>
-        <p className="w-full text-left text-xl md:text-3xl lg:text-4xl text-white/80">
+        <p className="w-full text-left text-2xl md:text-3xl lg:text-4xl text-white/80">
           {content.breadTextFour}
         </p>
       </div>

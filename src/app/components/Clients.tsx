c53c885eab8b3defff762d8e36e4b5fab604eb04
Allan Heremi <allanheremidev@gmail.com>
2023-12-10 12:43:26 +0100

    Fix: remove landscape

--- src/app/components/Clients.tsx
+++ src/app/components/Clients.tsx
@@ -1,56 +1,51 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { useMediaQuery } from "react-responsive";
 
 const Clients = () => {
-  const isLandscape = useMediaQuery({ query: "(orientation: landscape)" });
-
   return (
     <>
-      {!isLandscape && (
-        <div className="flex h-[66vh] grow grid-cols-2 flex-col justify-between bg-white p-8 text-black md:h-[50vh]">
-          <h1 className="flex justify-center py-8 text-5xl md:py-8 md:text-6xl lg:text-7xl">
-            CLIENTS
-          </h1>
-          <div className="flex grow items-center justify-center">
-            <div className="grid grid-cols-2 gap-x-24 gap-y-24 bg-white text-center text-black md:mb-4 md:grid-cols-3 md:gap-x-48 md:gap-y-4 lg:grid-cols-4">
-              <div>
-                <Image
-                  src={"/BK_Black_Logo.png"}
-                  width={94}
-                  height={94}
-                  alt="logo bored kitchen"
-                />
-              </div>
-              <div>
-                <Image
-                  src={"/Hairbyem2.png"}
-                  width={94}
-                  height={94}
-                  alt="logo bored kitchen"
-                />
-              </div>
-              <div>
-                <Image
-                  src={"/BK_Black_Logo.png"}
-                  width={94}
-                  height={94}
-                  alt="logo bored kitchen"
-                />
-              </div>
-              <div>
-                <Image
-                  src={"/Hairbyem2.png"}
-                  width={94}
-                  height={94}
-                  alt="logo bored kitchen"
-                />
-              </div>
+      <div className="flex h-[66vh] grow grid-cols-2 flex-col justify-between bg-white p-8 text-black md:h-[50vh]">
+        <h1 className="flex justify-center py-8 text-5xl md:py-8 md:text-6xl lg:text-7xl">
+          CLIENTS
+        </h1>
+        <div className="flex grow items-center justify-center">
+          <div className="grid grid-cols-2 gap-x-24 gap-y-24 bg-white text-center text-black md:mb-4 md:gap-x-48 lg:grid-cols-4">
+            <div>
+              <Image
+                src={"/BK_Black_Logo.png"}
+                width={94}
+                height={94}
+                alt="logo bored kitchen"
+              />
+            </div>
+            <div>
+              <Image
+                src={"/Hairbyem2.png"}
+                width={94}
+                height={94}
+                alt="logo bored kitchen"
+              />
+            </div>
+            <div>
+              <Image
+                src={"/BK_Black_Logo.png"}
+                width={94}
+                height={94}
+                alt="logo bored kitchen"
+              />
+            </div>
+            <div>
+              <Image
+                src={"/Hairbyem2.png"}
+                width={94}
+                height={94}
+                alt="logo bored kitchen"
+              />
             </div>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };

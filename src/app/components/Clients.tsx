44f47f905a9d0e009a1d40e081e666f7bcfcdc8d
Allan Heremi <allanheremidev@gmail.com>
2023-12-10 12:40:19 +0100

    Fix: resizing clients

--- src/app/components/Clients.tsx
+++ src/app/components/Clients.tsx
@@ -14,7 +14,7 @@ const Clients = () => {
             CLIENTS
           </h1>
           <div className="flex grow items-center justify-center">
-            <div className="grid grid-cols-2 gap-x-24 gap-y-24 bg-white text-center text-black md:mb-4 md:grid-cols-3 md:gap-x-48 md:gap-y-12 lg:grid-cols-4">
+            <div className="grid grid-cols-2 gap-x-24 gap-y-24 bg-white text-center text-black md:mb-4 md:grid-cols-3 md:gap-x-48 md:gap-y-4 lg:grid-cols-4">
               <div>
                 <Image
                   src={"/BK_Black_Logo.png"}

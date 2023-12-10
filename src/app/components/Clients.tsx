0ddb3d800731f85b1db3b6251e979365dae69e19
Allan Heremi <allanheremidev@gmail.com>
2023-12-10 11:57:59 +0100

    Fix: landscape orientation overflow

--- src/app/components/Clients.tsx
+++ src/app/components/Clients.tsx
@@ -1,180 +1,98 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+import { useMediaQuery } from "react-responsive";
 
 const Clients = () => {
+  const isLandscape = useMediaQuery({ query: "(orientation: landscape)" });
   return (
     <>
-      <div className="flex h-[66vh] grid-cols-2 flex-col justify-between bg-white p-8 text-black md:h-[50vh] grow">
-        <h1 className="flex justify-center py-8 text-5xl md:py-8 md:text-6xl lg:text-7xl">
-          CLIENTS
-        </h1>
-        <div className="flex grow items-center justify-center">
-          <div className="grid grid-cols-2 gap-x-24 gap-y-24 bg-white text-center text-black md:mb-4 md:grid-cols-2 md:gap-x-48 md:gap-y-12 lg:grid-cols-4">
-            <div>
-              <Image
-                src={"/BK_Black_Logo.png"}
-                width={94}
-                height={94}
-                alt="logo bored kitchen"
-              />
-            </div>
-            <div>
-              <Image
-                src={"/Hairbyem2.png"}
-                width={94}
-                height={94}
-                alt="logo bored kitchen"
-              />
-            </div>
-            <div>
-              <Image
-                src={"/BK_Black_Logo.png"}
-                width={94}
-                height={94}
-                alt="logo bored kitchen"
-              />
-            </div>
-            <div>
-              <Image
-                src={"/Hairbyem2.png"}
-                width={94}
-                height={94}
-                alt="logo bored kitchen"
-              />
-            </div>
-            {/* <div>
-              <Image
-                src={"/BK_Black_Logo.png"}
-                width={94}
-                height={72}
-                alt="logo bored kitchen"
-              />
-            </div>  */}
-            {/* <div>
-              <Image
-                src={"/Hairbyem2.png"}
-                width={72}
-                height={72}
-                alt="logo bored kitchen"
-              />
-            </div>
-            <div>
-              <Image
-                src={"/Hairbyem2.png"}
-                width={72}
-                height={72}
-                alt="logo bored kitchen"
-              />
-            </div>
-            <div>
-              <Image
-                src={"/BK_Black_Logo.png"}
-                width={72}
-                height={72}
-                alt="logo bored kitchen"
-              />
-            </div>
-            <div>
-              <Image
-                src={"/Hairbyem2.png"}
-                width={72}
-                height={72}
-                alt="logo bored kitchen"
-              />
-            </div> */}
-          </div>
-        </div>
-      </div>
-
-      {/* {!isDesktopOrLaptop && (
-        <div className="bg-white text-black">
-          <div className="p-4 py-4">
-            <h1 className="p-4 pb-8 text-4xl">OUR PARTNERS</h1>
-
-            <div className="object-full w-full p-4 pb-4">
-              <Image
-                src={"/boredkitchen2.png"}
-                width={300}
-                height={300}
-                alt="boredkitchen"
-                className="h-48 w-full object-cover"
-              />
-              <h3 className="py-2 text-xl">BORED KITCHEN</h3>
-            </div>
-          </div>
-
-          <div className="gap-y-4 p-4 py-4">
-            <div className="object-full w-full p-4 pb-4 pt-0">
-              <Image
-                src={"/boredkitchen3.png"}
-                width={300}
-                height={300}
-                alt="boredkitchen"
-                className="h-48 w-full object-cover"
-              />
-              <h3 className="py-2 text-xl">BORED KITCHEN</h3>
-            </div>
-          </div>
-
-          <div className="gap-y-4 p-4 pb-4">
-            <div className="object-full w-full p-4 pb-4">
-              <Image
-                src={"/boredkitchen1.png"}
-                width={300}
-                height={400}
-                alt="boredkitchen"
-                className="h-48 w-full object-cover"
-              />
-              <h3 className="py-2 text-xl">BORED KITCHEN</h3>
+      {!isLandscape && (
+        <div className="flex h-[66vh] grow grid-cols-2 flex-col justify-between bg-white p-8 text-black md:h-[50vh]">
+          <h1 className="flex justify-center py-8 text-5xl md:py-8 md:text-6xl lg:text-7xl">
+            CLIENTS
+          </h1>
+          <div className="flex grow items-center justify-center">
+            <div className="grid grid-cols-2 gap-x-24 gap-y-24 bg-white text-center text-black md:mb-4 md:grid-cols-2 md:gap-x-48 md:gap-y-12 lg:grid-cols-4">
+              <div>
+                <Image
+                  src={"/BK_Black_Logo.png"}
+                  width={94}
+                  height={94}
+                  alt="logo bored kitchen"
+                />
+              </div>
+              <div>
+                <Image
+                  src={"/Hairbyem2.png"}
+                  width={94}
+                  height={94}
+                  alt="logo bored kitchen"
+                />
+              </div>
+              <div>
+                <Image
+                  src={"/BK_Black_Logo.png"}
+                  width={94}
+                  height={94}
+                  alt="logo bored kitchen"
+                />
+              </div>
+              <div>
+                <Image
+                  src={"/Hairbyem2.png"}
+                  width={94}
+                  height={94}
+                  alt="logo bored kitchen"
+                />
+              </div>
             </div>
           </div>
         </div>
       )}
-
-      {isDesktopOrLaptop &&
-              <div className="bg-white text-black">
-                <h1 className="p-4 pb-8 text-4xl">OUR PARTNERS</h1>
-              <div className="p-4 py-4 flex flex-row">
-                <div className="object-full w-full p-4 pb-4 flex items-center p-8">
-                  <Image
-                    src={"/boredkitchen2.png"}
-                    width={200}
-                    height={200}
-                    alt="boredkitchen"
-                    className="h-48 w-1/2 object-cover"
-                  />
-                  <h3 className="py-2 w-1/2 text-xl justify-center text-center underline">BORED KITCHEN</h3>
-                </div>
+      {isLandscape && (
+        <div className="flex h-[66vh] grow grid-cols-2 flex-col justify-between bg-white p-8 text-black md:h-[50vh]">
+          <h1 className="flex justify-center py-8 text-5xl md:py-8 md:text-6xl lg:text-7xl">
+            CLIENTS
+          </h1>
+          <div className="flex grow items-center justify-center">
+            <div className="grid grid-cols-2 gap-x-24 gap-y-24 bg-white text-center text-black md:mb-4 md:grid-cols-4 md:gap-x-48 md:gap-y-12 lg:grid-cols-4">
+              <div>
+                <Image
+                  src={"/BK_Black_Logo.png"}
+                  width={94}
+                  height={94}
+                  alt="logo bored kitchen"
+                />
               </div>
-    
-              <div className="p-4 py-4 flex flex-row">
-                <div className="object-cover w-full p-4 pb-4 flex items-center">
-                <h3 className="py-2 w-1/2 text-xl justify-center text-center underline">BORED KITCHEN</h3>
-                  <Image
-                    src={"/boredkitchen2.png"}
-                    width={200}
-                    height={200}
-                    alt="boredkitchen"
-                    className="h-48 w-1/2 object-cover"
-                  />
-                 
-                </div>
+              <div>
+                <Image
+                  src={"/Hairbyem2.png"}
+                  width={94}
+                  height={94}
+                  alt="logo bored kitchen"
+                />
               </div>
-    
-              <div className="p-4 py-4 flex flex-row">
-                <div className="object-full w-full p-4 pb-4 flex items-center">
-                  <Image
-                    src={"/boredkitchen2.png"}
-                    width={200}
-                    height={200}
-                    alt="boredkitchen"
-                    className="h-48 w-1/2 object-cover"
-                  />
-                  <h3 className="py-2 w-1/2 text-xl justify-center text-center underline">BORED KITCHEN</h3>
-                </div>
+              <div>
+                <Image
+                  src={"/BK_Black_Logo.png"}
+                  width={94}
+                  height={94}
+                  alt="logo bored kitchen"
+                />
               </div>
-            </div> */}
+              <div>
+                <Image
+                  src={"/Hairbyem2.png"}
+                  width={94}
+                  height={94}
+                  alt="logo bored kitchen"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };

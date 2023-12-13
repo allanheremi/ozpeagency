a8c548ccb6eadbed4c34c738bf032c0079788fe4
Allan Heremi <allanheremidev@gmail.com>
2023-12-13 14:58:01 +0100

    Fix: text content

--- src/app/components/Heading.tsx
+++ src/app/components/Heading.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Heading = () => {
   return (
     <h1 className='p-4 lg:text-7xl text-6xl flex items-center px-8 md:px-24 h-[88vh]'>
-THE CREATIVE AGENCY THAT BREAKS RULES.
+A CREATIVE AGENCY THAT KNOWS NOTHING
+BUT IMAGINATION.
     </h1>
   )
 }
--- src/app/components/Subheading.tsx
+++ src/app/components/Subheading.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Subheading = () => {
   return (
     <div>
-      <h1 className="flex h-[100vh] items-center bg-white p-4 px-8 text-6xl text-black md:px-24 lg:text-7xl">
-        IMAGINATION WAS ALWAYS THE EDGE, TEXT TO BE IMPLEMENTED TEXT TO BE
+      <h1 className="flex h-[100vh] items-center bg-white p-4 px-8 text-2xl text-black md:text-3xl md:px-24 lg:text-3xl sm:text-2xl">
+        THIS IS NOT EMPTY WHITE SPACE, ITS FULL OF THINGS
       </h1>
     </div>
   );

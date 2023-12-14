12f23caf155340f7b2339c1c791cd1fe37d266ac
Allan Heremi <allanheremidev@gmail.com>
2023-12-14 13:40:36 +0100

    Fix: storage token

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-const token = process.env.FIREBASE_STORAGE_TOKEN;
 
 const Main = () => {
   return (
     <video
       autoPlay
       muted
-      controlsList="nofullscreen nodownload noremoteplayback noplaybackrate"
+      controlsList="nofullscreen nodownload noremoteplayback noplaybackrate" 
       disablePictureInPicture
       loop={true}
       controls
       className="w-screen"
     >
       <source
-        src={`https://firebasestorage.googleapis.com/v0/b/ozpeagency-27bcf.appspot.com/o/website_film.mp4?alt=media&token=${token}`}
+        src="https://firebasestorage.googleapis.com/v0/b/ozpeagency-27bcf.appspot.com/o/website_film.mp4?alt=media&token=b11d833b-9a0a-426d-aed1-69b30e86398b"
         type="video/mp4"
       />
       Your browser does not support this video format

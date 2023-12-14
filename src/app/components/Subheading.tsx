f46fe8105e04c665857d1aa7af6aac897ad9e54f
Allan Heremi <allanheremidev@gmail.com>
2023-12-14 12:43:00 +0100

    Fix: subheading content

--- src/app/components/Subheading.tsx
+++ src/app/components/Subheading.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Subheading = () => {
   return (
     <div>
-      <h1 className="flex h-[100vh] items-center justify-center bg-white p-4 px-8 text-2xl text-black md:text-3xl md:px-24 lg:text-3xl">
-        THIS IS NOT EMPTY WHITE SPACE (ITS FULL OF THINGS)
+      <h1 className="flex h-[100vh] items-center justify-center bg-white p-4 px-8 text-1xl text-black md:text-2xl md:px-24 lg:text-2xl">
+      (this is not empty white space, its full of things)
       </h1>
     </div>
   );
